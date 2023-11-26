1d0482cacf28b7872778ea2d9b03bbcd94862884
kotathefriends <5070kouta@gmail.com>
2023-11-26 15:34:31 +0900

    Import Sample Images

--- ChatViewWithSwiftUI/Views/MessageRow.swift
+++ ChatViewWithSwiftUI/Views/MessageRow.swift
@@ -10,33 +10,48 @@ import SwiftUI
 struct MessageRow: View {
     var body: some View {
         HStack(alignment: .top) {
-            Image(systemName: "person.circle")
-                .resizable()
-                .frame(width: 48, height: 48)
-            Text("こんにちは！")
-                .padding()
-                .background(.white)
-                .cornerRadius(30)
-            VStack(alignment: .trailing) {
-                Spacer()
-                Text("既読")
-                Text(formattedDataString)
-            }
-            .foregroundColor(.secondary)
-            .font(.footnote)
+            userThumb
+            messageText
+            messageState
             Spacer()
         }
         .padding(.bottom)
-    }
-    
-    private var formattedDataString: String {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        return formatter.string(from: Date())
     }
 }
 
 #Preview {
     MessageRow()
         .background(.cyan)
+}
+
+extension MessageRow {
+    
+    private var userThumb: some View {
+        Image("user01")
+            .resizable()
+            .frame(width: 48, height: 48)
+            .clipShape(Circle())
+    }
+    
+    private var messageText: some View {
+        Text("こんにちは！")
+            .padding()
+            .background(.white)
+            .cornerRadius(30)
+    }
+    
+    private var messageState: some View {
+        VStack(alignment: .trailing) {
+            Spacer()
+            Text("既読")
+            Text(formattedDataString)
+        }
+        .foregroundColor(.secondary)
+        .font(.footnote)
+    }
+    private var formattedDataString: String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: Date())
+    }
 }
