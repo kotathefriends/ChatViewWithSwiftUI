9bd202670efdf74627c98ee0333aa1d9582a0a42
kotathefriends <5070kouta@gmail.com>
2023-11-26 12:21:45 +0900

    Initial Commit

---
+++ ChatViewWithSwiftUI/ChatViewWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewWithSwiftUIApp.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by KOTA TAKAHASHI on 2023/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
